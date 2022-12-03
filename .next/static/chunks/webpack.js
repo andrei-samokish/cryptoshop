d26cd9ff7a66671dab22e489e110a05bf9ca07a5
4ndrei <andreygames5@mail.ru>
2022-12-03 22:16:57 +0700

    minor

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e71c08c634f8684b"; }
+/******/ 		__webpack_require__.h = function() { return "3a01a36fd298fcae"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/account.tsx
+++ pages/account.tsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react";
 import { Button } from "semantic-ui-react";
 import contrWithSigner from "../components/connectProvider/contrWithSigner";
 import Layout from "../components/Layout";
-import mainContr from "../components/mainContr";
 import OwnedItemsRender from "../components/OwnedItemsRender";
 import SubmitForSalePortal from "../components/SubmitForSalePortal";
 
@@ -25,7 +24,7 @@ function Account() {
 
   async function becomeSellerClick() {
     try {
-      await (await contrWithSigner()).becomeSeller({ gasLimit: 50000 });
+      await (await contrWithSigner()).becomeSeller({ gasLimit: 150000 });
     } catch (error) {
       console.error(error);
     }
@@ -37,7 +36,7 @@ function Account() {
       return <Button onClick={() => setOpen(true)}>Submit item for sale</Button>;
     else
       return (
-        <Button primary onClick={becomeSellerClick}>
+        <Button primary onClick={async () => await becomeSellerClick()}>
           Become seller
         </Button>
       );
