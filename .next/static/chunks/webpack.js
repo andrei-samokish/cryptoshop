b673c57c999dd3c4d2019d8820c6739452d13202
4ndrei <andreygames5@mail.ru>
2022-12-07 17:50:54 +0700

    7.12.2022

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3a01a36fd298fcae"; }
+/******/ 		__webpack_require__.h = function() { return "5234243414faccfe"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
