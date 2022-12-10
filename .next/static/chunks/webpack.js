184908b73b64d0dbfaecf82f7cbd24a65cec2fe0
4ndrei <andreygames5@mail.ru>
2022-12-10 20:00:18 +0700

    gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "658d894490556757"; }
+/******/ 		__webpack_require__.h = function() { return "773f756dbf5f47db"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
