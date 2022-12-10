0c3f2b425c8c34fedec111fdb6bab10de8a56d0c
4ndrei <andreygames5@mail.ru>
2022-12-10 20:23:11 +0700

    visual fix

--- components/OwnedCard.tsx
+++ components/OwnedCard.tsx
@@ -31,50 +31,52 @@ export default function OwnedCard({ item, amount }: CardProps) {
     }
   }
   return (
-    <Card key={uuidv4()}>
-      <Image src={`data:image/svg+xml;base64,${item.img}`} wrapped ui={false} />
-      <Card.Content>
-        <Card.Header>{item.name}</Card.Header>
-        <Card.Meta>{ethers.utils.formatEther(item.price) + "Ξ"}</Card.Meta>
-        <Card.Description>You have {amount} of those!</Card.Description>
-      </Card.Content>
-      {isSeller && (
-        <Card.Content extra>
-          <Grid columns={2}>
-            <Grid.Column stretched>
-              <Popup
-                trigger={<Button content="Withdraw from sale" basic />}
-                content={
-                  <Button
-                    color="red"
-                    content="Sure?"
-                    onClick={() => handleWithdrawClick(item.id)}
-                  />
-                }
-                on="click"
-                position="top right"
-              />
-            </Grid.Column>
-            <Grid.Column stretched>
-              <Popup
-                trigger={<Button content="Fill stock" basic />}
-                content={
-                  <Input
-                    placeholder="amount"
-                    type="text"
-                    action
-                    onChange={(e) => setFillAmount(parseInt(e.target.value))}>
-                    <input />
-                    <Button onClick={() => handleFillClick(item.id)}>Fill</Button>
-                  </Input>
-                }
-                on="click"
-                position="top right"
-              />
-            </Grid.Column>
-          </Grid>
+    <div className="w-1/3">
+      <Card key={uuidv4()}>
+        <Image src={`data:image/svg+xml;base64,${item.img}`} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>{item.name}</Card.Header>
+          <Card.Meta>{ethers.utils.formatEther(item.price) + "Ξ"}</Card.Meta>
+          <Card.Description>You have {amount} of those!</Card.Description>
         </Card.Content>
-      )}
-    </Card>
+        {isSeller && (
+          <Card.Content extra>
+            <Grid columns={2}>
+              <Grid.Column stretched>
+                <Popup
+                  trigger={<Button content="Withdraw from sale" basic />}
+                  content={
+                    <Button
+                      color="red"
+                      content="Sure?"
+                      onClick={() => handleWithdrawClick(item.id)}
+                    />
+                  }
+                  on="click"
+                  position="top right"
+                />
+              </Grid.Column>
+              <Grid.Column stretched>
+                <Popup
+                  trigger={<Button content="Fill stock" basic />}
+                  content={
+                    <Input
+                      placeholder="amount"
+                      type="text"
+                      action
+                      onChange={(e) => setFillAmount(parseInt(e.target.value))}>
+                      <input />
+                      <Button onClick={() => handleFillClick(item.id)}>Fill</Button>
+                    </Input>
+                  }
+                  on="click"
+                  position="top right"
+                />
+              </Grid.Column>
+            </Grid>
+          </Card.Content>
+        )}
+      </Card>
+    </div>
   );
 }
